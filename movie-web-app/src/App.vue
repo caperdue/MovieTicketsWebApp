558f1214f58ba318a38360df94fff17519cff95d
sophiejo32 <sophiejo32@gmail.com>
2021-03-25 17:22:35 -0400

    added your movie component

--- movie-web-app/src/App.vue
+++ movie-web-app/src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <Tickets v-for="(c,pos) in ticketsInfo.movies" :key="pos"
   v-bind:movieName="c.movieName" v-bind:ticketNumber="c.ticketNumber" v-bind:theatreNumber="c.theatreNumber" v-bind:seatNumber="c.seatNumber" v-bind:movieRating="c.movieRating" v-bind:movieDate="c.movieDate" v-bind:imageurl="imgurl" ></Tickets><br>
+  <Movie :movie-name="movies.movieTitle" :movie-time="movies.movieTimes" :movieImgUrl='movies.movieImgUrl' />
+
   </div>
 </template>
 
@@ -9,15 +11,23 @@
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from './components/HelloWorld.vue';
 import Tickets from './components/Tickets.vue';
+import Movie from './components/Movie.vue';
 
 
 @Component({
   components: {
-    HelloWorld, Tickets
+    HelloWorld, Tickets, Movie
   },
 })
 export default class App extends Vue {
-readonly imgurl =  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/800px-QR_code_for_mobile_English_Wikipedia.svg.png"
+
+  readonly movies = { 
+    movieTimes: ["11:30pm", "12:00pm"],
+    movieTitle: "The Joker",
+    movieImgUrl: "https://fanart.tv/fanart/movies/475557/movieposter/joker-5d6732e7bf212.jpg"
+  }
+
+  readonly imgurl =  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/800px-QR_code_for_mobile_English_Wikipedia.svg.png"
 
   readonly ticketsInfo = {
   movies: [
---
+++ movie-web-app/src/components/Movie.vue
@@ -0,0 +1,93 @@
+<template>
+<div class="container">
+  <div id="title">
+    <div id="information">
+      <h1>{{movieName}}</h1>
+      <p>Rated R</p>
+    </div>
+    <div>
+      <div>
+        <img :src="movieImgUrl"/>
+        <p>Description textDescrkhlsdfkgjhdfkg kjfhsdfkjhj</p>
+      </div>
+    </div>
+  </div>
+  <div id="times"> 
+    <h2>Times</h2>
+    <ul>
+      <li v-for="item in movieTime" :key="item">
+        {{item}}
+      </li>
+    </ul>
+  </div>
+</div>
+ 
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+
+@Component
+export default class Movie extends Vue {
+  @Prop() private movieName!: string;
+  @Prop() private movieImgUrl!: string;
+  @Prop() private movieTime!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.container {
+  overflow:hidden;
+  padding:0;
+  width: 60%;
+  font-size: 1rem;
+}
+
+ul {
+  list-style-type: none;
+  border:1px solid rgba(0,0,0, 1);
+  padding: 0;
+  display:inline-block;
+  text-align: center;
+
+}
+li {
+  padding: 15px;
+}
+li:hover {
+  background-color: rgba(0,0,0, 0.1);
+}
+
+.container #title {
+  padding:15px;
+  margin:0;
+  display:inline-block;
+  width:75%;
+  text-align:left;
+  border-left: 30px solid red;
+}
+.container #times {
+  padding:25px;
+  float:right;
+  width: 25%;
+}
+
+
+.container #title #information p {
+  text-align:right;
+  border-bottom: 1px solid rgba(0,0,0, 0.1)
+}
+img {
+  width:40%;
+  padding-right:10px;
+  float:left;
+}
+h1 {
+  border-bottom: 1px solid rgba(0,0,0, 0.1)
+}
+
+
+</style>
